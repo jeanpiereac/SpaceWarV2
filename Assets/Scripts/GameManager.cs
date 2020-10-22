235578b42d201e419352f200af92ad229ba1dd2e
Jeanpiere Alcocer <jeanpiere_alcocer@usmp.pe>
2020-10-21 21:52:27 -0500

    Versión Final

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject nave;
+    public GameObject win;
+    public GameObject astro;
+    public Text TextScore;
+    int score=0;
+    bool winW=false;
+    bool astroW=false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn",1f,1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (winW == true && astroW == true)
+        {
+            CancelInvoke("Spawn");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+    void Spawn()
+    {
+        float randomX = Random.Range(62f,955f);
+        float randomY = Random.Range(61f, 568f); ;
+
+        Vector3 randomPosition = new Vector3(randomX, randomY, 0);
+
+        Instantiate(nave,
+            randomPosition, Quaternion.identity);
+    }
+
+    public void IncrementScore()
+    {
+        score++;
+        TextScore.text = score.ToString();
+
+        if (score >= 20)
+        {
+            winW = true;
+            astroW = true;
+            win.SetActive(winW);
+            astro.SetActive(astroW);
+        }
+
+    }
+
+
+}
---
+++ Assets/Scripts/NaveBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaveBehaviour : MonoBehaviour
+{
+
+    public GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        Destroy(gameObject, 3f);
+    }
+    public void OnMouseDown()
+    {
+        gameManager.IncrementScore();
+        Destroy(gameObject);
+    }
+   
+}
